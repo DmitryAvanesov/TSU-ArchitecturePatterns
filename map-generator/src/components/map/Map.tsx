abe18dc82465815aeaf842bd8a3dd4696ee307eb
Dima <dima.avanesov@yandex.ru>
2020-10-14 20:11:11 +0700

    CRA for map generator + TS issue fix

---
+++ map-generator/src/components/map/Map.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Map.css";
+import Map from "./Map";
+
+function MapModel() {
+  const map = new Map();
+
+  return <div className="map"></div>;
+}
+
+export default MapModel;
